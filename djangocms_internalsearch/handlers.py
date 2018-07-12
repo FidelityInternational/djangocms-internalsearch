5bff0d0ea40bef47d7202073f261be05c984fecd
Rizwan Mansuri <mr.mansuri@gmail.com>
2018-07-12 13:38:20 +0100

    Removed unncessory argument and passing only instance for save_to_index method

--- djangocms_internalsearch/handlers.py
+++ djangocms_internalsearch/handlers.py
@@ -7,4 +7,5 @@ def update_index(sender, operation, request, token, **kwargs):
     config = apps.get_app_config('djangocms_internalsearch')
     internalsearch_models = config.cms_extension.internalsearch_models
     if sender in internalsearch_models:
-        save_to_index(sender, operation, kwargs)
+        instance = kwargs['new_plugin']
+        save_to_index(instance)
