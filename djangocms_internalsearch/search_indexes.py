1ee77568a23735316d3f5773ce52d67680fc8dc7
Sravan Jinna <sravan.jinna@fil.com>
2018-07-16 09:35:28 +0100

    to create a PR to FIL/ class factory code

--- djangocms_internalsearch/search_indexes.py
+++ djangocms_internalsearch/search_indexes.py
@@ -10,20 +10,20 @@ apps.py file.
 '''
 
 
-class PageSearchIndex(indexes.SearchIndex, indexes.Indexable):
-
-    text = indexes.CharField(document=True)
-    created_by = indexes.CharField(model_attr="created_by", faceted=True)
-    placeholders = indexes.MultiValueField()
-
-    def get_model(self):
-        return Page
-
-    def index_queryset(self, using=None):
-        return self.get_model().objects.all()
-
-    def prepare_placeholders(self, object):
-        return [placeholders.slot for placeholders in object.placeholders.all()]
+# class PageSearchIndex(indexes.SearchIndex, indexes.Indexable):
+#
+#     text = indexes.CharField(document=True)
+#     created_by = indexes.CharField(model_attr="created_by", faceted=True)
+#     placeholders = indexes.MultiValueField()
+#
+#     def get_model(self):
+#         return Page
+#
+#     def index_queryset(self, using=None):
+#         return self.get_model().objects.all()
+#
+#     def prepare_placeholders(self, object):
+#         return [placeholders.slot for placeholders in object.placeholders.all()]
 
 
 # class PlaceholderSearchIndex(indexes.SearchIndex, indexes.Indexable):
@@ -38,25 +38,25 @@ class PageSearchIndex(indexes.SearchIndex, indexes.Indexable):
 #         return self.get_model().objects.all().values()
 
 
-# def gen_classes(received_class):
-#
-#     text = indexes.CharField(document=True)
-#
-#     def get_model(self):
-#         return received_class
-#
-#     def prepare_text(self, obj):
-#         return "{}".format(obj.plugin_type)
-#
-#     klass_name = received_class.__name__+'SearchIndex'
-#     klass_dict = {'text': text, 'get_model': get_model, 'prepare_text': prepare_text}
-#     klass = type(klass_name, (indexes.SearchIndex, indexes.Indexable), klass_dict)
-#
-#     def klass__init(self):
-#         super(klass, self).__init__()
-#
-#     setattr(klass, "__init__", klass__init)
-#     this_mod[klass_name] = klass
+def gen_classes(received_class):
+
+    text = indexes.CharField(document=True)
+
+    def get_model(self):
+        return received_class
+
+    def prepare_text(self, obj):
+        return "{}".format(obj.plugin_type)
+
+    klass_name = received_class.__name__+'SearchIndex'
+    klass_dict = {'text': text, 'get_model': get_model, 'prepare_text': prepare_text}
+    klass = type(klass_name, (indexes.SearchIndex, indexes.Indexable), klass_dict)
+
+    def klass__init(self):
+        super(klass, self).__init__()
+
+    setattr(klass, "__init__", klass__init)
+    this_mod[klass_name] = klass
 
 
 
