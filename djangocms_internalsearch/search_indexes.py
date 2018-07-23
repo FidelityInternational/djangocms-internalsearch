3603c4dfd22aa7fcd11fea1c1fbca45ecbe37590
Sravan Jinna <sravan.jinna@fil.com>
2018-07-23 16:54:57 +0100

    changes based on review and some more tests

--- djangocms_internalsearch/search_indexes.py
+++ djangocms_internalsearch/search_indexes.py
@@ -4,32 +4,30 @@ from collections import Iterable
 from haystack import indexes
 
 
-this_mod = globals()
-
-'''
-The below creates a class for haystack to index 
-particular model which we have passed in our 
-apps.py file.
-'''
+this_mod = locals()
 
 
 def generate_search_index_classes(model_list):
-
+    """
+    The below creates a class for haystack to index
+    particular model which we have passed in our
+    apps.py file.
+    """
     if not isinstance(model_list, Iterable):
-        raise TypeError("expects a list or tuple")
+        raise TypeError("generate_search_index_classes expects a list or tuple")
 
-    for model_class in model_list:
+    for model in model_list:
 
-        if not inspect.isclass(model_class):
-            raise TypeError("not a class object")
+        if not inspect.isclass(model):
+            raise TypeError("model is not a class object")
 
-        index_class_name = model_class.__name__ + 'Index'
-        index_class_created = class_factory(index_class_name, model_class)
+        index_class_name = model.__name__ + 'Index'
+        index_class_created = class_factory(index_class_name, model)
 
         this_mod[index_class_name] = index_class_created
 
         if not inspect.isclass(index_class_created):
-            raise TypeError("not a object instance")
+            raise TypeError("created index_class_created is not a object instance")
 
 
 class BaseClass:
@@ -37,7 +35,7 @@ class BaseClass:
         self._type = class_type
 
 
-def class_factory(name, model_class, BaseClass=BaseClass):
+def class_factory(name, model, BaseClass=BaseClass):
 
     def __init__(self):
         BaseClass.__init__(self, name)
@@ -45,7 +43,7 @@ def class_factory(name, model_class, BaseClass=BaseClass):
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
-        return model_class
+        return model
 
     index_class_dict = {"__init__": __init__, "text": text, "get_model": get_model}
     new_class = type(name, (BaseClass, indexes.SearchIndex, indexes.Indexable), index_class_dict)
--- tests/test_class_factory.py
+++ tests/test_class_factory.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
+from django.apps import apps
 
 from cms import app_registration
+from cms.test_utils.testcases import CMSTestCase
+from cms.utils.setup import setup_cms_apps
+
 from djangocms_internalsearch.search_indexes import (
     generate_search_index_classes,
 )
@@ -25,6 +29,23 @@ class ClassFactoryUnitTestCase(TestCase):
     def test_passing_classes(self):
         test_class_list = [TestModel1, TestModel2]
         generate_search_index_classes(test_class_list)
-        self.assertEqual(TestModel1.__name__, 'TestModel1')
         from djangocms_internalsearch.search_indexes import TestModel1Index
         self.assertEqual(TestModel1Index.__name__, 'TestModel1Index')
+
+
+class InternalSearchIntegrationTestCase(CMSTestCase):
+
+    def setUp(self):
+        app_registration.get_cms_extension_apps.cache_clear()
+        app_registration.get_cms_config_apps.cache_clear()
+
+    def test_config_with_two_apps(self):
+        setup_cms_apps()
+        internalsearch_config = apps.get_app_config('djangocms_internalsearch')
+        model_list = internalsearch_config.cms_extension.internalsearch_models
+        generate_search_index_classes(model_list)
+        from djangocms_internalsearch.search_indexes import TestModel1Index, TestModel2Index, TestModel3Index, TestModel4Index
+        self.assertEqual(TestModel1Index.__name__, 'TestModel1Index')
+        self.assertEqual(TestModel2Index.__name__, 'TestModel2Index')
+        self.assertEqual(TestModel3Index.__name__, 'TestModel3Index')
+        self.assertEqual(TestModel4Index.__name__, 'TestModel4Index')
