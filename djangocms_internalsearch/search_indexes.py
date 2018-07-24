42c6a368471b545f83526600390abe7742a99f64
Sravan Jinna <sravan.jinna@fil.com>
2018-07-24 11:48:31 +0100

    changes based on Matus review for PR

--- djangocms_internalsearch/search_indexes.py
+++ djangocms_internalsearch/search_indexes.py
@@ -8,11 +8,9 @@ this_mod = locals()
 
 
 def generate_search_index_classes(model_list):
-    """
-    The below creates a class for haystack to index
-    particular model which we have passed in our
-    apps.py file.
-    """
+
+    # The below creates a class for haystack to index particular model which we have passed in our apps.py file.
+
     if not isinstance(model_list, Iterable):
         raise TypeError("Generate method expects a list or tuple")
 
@@ -27,20 +25,20 @@ def generate_search_index_classes(model_list):
         this_mod[index_name] = index_created
 
         if not inspect.isclass(index_created):
-            raise TypeError("Created index_class_created is not a object instance")
+            raise TypeError("Created search index is not a object instance")
 
 
 class BaseClass:
-    def __init__(self, class_type):
-        self._type = class_type
+    def __init__(self, name):
+        self._type = name
 
 
 def class_factory(name, model, BaseClass=BaseClass):
 
+    text = indexes.CharField(document=True, use_template=True)
+
     def __init__(self):
         BaseClass.__init__(self, name)
-
-    text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
         return model
