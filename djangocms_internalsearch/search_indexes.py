6c26bc83a8e9bcdd847789ca11013a4a974c4042
Sravan Jinna <sravan.jinna@fil.com>
2018-07-31 16:43:23 +0100

    added the unified_index logic to add search indexes to haystack

--- djangocms_internalsearch/search_indexes.py
+++ djangocms_internalsearch/search_indexes.py
@@ -1,10 +1,9 @@
 from django.core.exceptions import FieldDoesNotExist
 
-
 from haystack import indexes
 
 
-def create_indexes(model_config_list):
+def create_indexes(model_config_list, internalsearch_indexes):
     """
     The below creates classes for haystack to index particular
     model which we have passed in our apps.py file.
@@ -12,13 +11,16 @@ def create_indexes(model_config_list):
 
     for config in model_config_list:
 
-        class_name = config.model.__name__ + 'Index'
-        search_index_class = class_factory(class_name, config)
+        search_index_class = None
+        if config.index is None:
+            search_index_class = class_factory(config)
+        else:
+            search_index_class = config.index
 
-        globals()[class_name] = search_index_class
+        internalsearch_indexes.unified_index._indexes[config.model] = search_index_class
 
 
-def class_factory(name, config):
+def class_factory(config):
     text = indexes.CharField(document=True)
 
     def get_model(self):
@@ -26,22 +28,19 @@ def class_factory(name, config):
 
     index_class_dict = {"text": text, "get_model": get_model}
 
-    if config.index is None:
-        for field in config.fields:
-            f = None
-            try:
-                f = config.model._meta.get_field(field)
-            except FieldDoesNotExist:
-                raise
-            if f:
-                index_class_dict[f.name] = indexes.index_field_from_django_field(f)(
-                    model_attr=f.name)
-    else:
-        # TODO; add the fields we get from config
-        pass
+    for field in config.fields:
+        f = None
+        try:
+            f = config.model._meta.get_field(field)
+        except FieldDoesNotExist:
+            raise
+        if f:
+            index_class_dict[f.name] = indexes.index_field_from_django_field(f)(
+                model_attr=f.name)
 
+    class_name = config.model.__name__ + 'Index'
     new_class = type(
-        name,
+        class_name,
         (indexes.SearchIndex, indexes.Indexable),
         index_class_dict
     )
